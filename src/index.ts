4f2332a56e83569cc57abd233880c0d645c01e77
Hehsam saraya <heshamsaraya41@gmail.com>
2024-07-14 10:41:09 +0200

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import path from "path";
-import express from "express";
-import { Request, Response, NextFunction } from "express";
-
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config({ path: "config.env" });
 import i18n from "./i18n";
@@ -11,8 +9,6 @@ import createTables from "./databaseConfig/tables";
 import mountRoutes from "./hooks/hooks";
 import { seedUsers } from "./sedeer";
 
-//conncet with db
-dbConnect();
 
 // seedUsers()
 
@@ -29,18 +25,19 @@ mountRoutes(app);
 const PORT = process.env.PORT || 3000;
 
 app.get("*", (req: Request, res: Response) => {
-  res.status(505).json({ message: "Bad Request" });
+  res.status(404).json({ message: "Bad Request" });
 });
 
 const start = async (): Promise<void> => {
   try {
+    await dbConnect();
     await createTables();
 
     app.listen(PORT, () => {
       console.log(`App running on port ${PORT}`);
     });
   } catch (error) {
-    console.log(error);
+    console.log("Failed to start the server:", error);
     process.exit(1);
   }
 };
